9ee26b723d12b56d784183fb9cd3862490140a9c
nabin <nkbhaie2017@gmail.com>
2023-09-02 00:43:05 -0600

    add classes in java

--- src/oops/ClassesAndObjects.java
+++ src/oops/ClassesAndObjects.java
@@ -1,5 +1,20 @@
 package oops;
 
-public class ClassesAndObjects {
+//create a class and instantiate it in public class
+class ProgrammingLanguages{
+	//public static String getLanguages() { // works
+	//public String getLanguages() { // works
+	String getLanguages() { // only return type and function name
+		System.out.println("This is from class to be instantiated...");
+		return "Languages are Java, JavaScript, Python, and C++.";
+	}
+}
 
+public class ClassesAndObjects {
+	public static void main(String[] args) {
+		// Instantiate the ProgrammingLanguages class - means create its object
+		ProgrammingLanguages pl = new ProgrammingLanguages();
+		String result1 = pl.getLanguages();
+		System.out.println(result1);
+	}
 }
