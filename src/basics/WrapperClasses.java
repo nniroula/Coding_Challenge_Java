f009a2c4a7028a8773c4055c812f168233b0f64c
nabin <nkbhaie2017@gmail.com>
2023-09-02 01:32:26 -0600

    add some wrapper classes

--- src/basics/WrapperClasses.java
+++ src/basics/WrapperClasses.java
@@ -1,5 +1,22 @@
 package basics;
 
-public class WrapperClasses {
+/*
+ * Integer is a Wrapper class for 'int' primitive data type.
+ * Wrapper class converts corresponding primitive data types to a reference type
+ * int and Integer,   long and Long, byte and Byte, boolean and Boolean,
+ * char and Character, float and Float, double and Double
+ *
+ */
 
+public class WrapperClasses {
+	public static void main(String []args) {
+		// use Wrapper classes
+		Integer age = 27;
+		Double gpa = 3.8;
+		Character ch = 'A';
+		
+		age.intValue(); // has different methods
+		System.out.println(age.intValue()); //both work fine
+		System.out.println(age);
+	}
 }
