8a0941d74b10aabaff6dc010b3eb6e672dfe51a8
nabin <nkbhaie2017@gmail.com>
2023-09-04 11:59:25 -0600

    add some loops

--- src/basics/Loops.java
+++ src/basics/Loops.java
@@ -1,5 +1,36 @@
 package basics;
 
+class DifferentLoops{
+	public void tradidtionForLoop() {
+		for(int i = 0; i< 10; i++) {
+			System.out.println(i);
+		}
+	}
+	
+	// while loop
+	void getWhileLoop() {
+		int count = 1;
+		while(count < 6) {
+			System.out.println("Language is JAVA!!!");
+			count += 1;
+		}
+	}
+	
+	// enhanced for loop OR forEach loop
+	
+	// do- while loop
+}
+	
+	
 public class Loops {
-
+	public static void main(String args[]) {
+		// instantiate DifferentLoop class
+		DifferentLoops dl = new DifferentLoops();
+		
+		// traditionalForLoop method
+		dl.tradidtionForLoop(); 
+		
+		// while loop method
+		dl.getWhileLoop();
+	}
 }
