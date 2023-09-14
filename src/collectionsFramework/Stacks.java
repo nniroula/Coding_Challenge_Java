fcf36cdc7ab565f129cc2d854ccf570bdbf09ef9
nabin <nkbhaie2017@gmail.com>
2023-09-13 21:13:59 -0600

    add some stack methods

--- src/collectionsFramework/Stacks.java
+++ src/collectionsFramework/Stacks.java
@@ -1,5 +1,26 @@
 package collectionsFramework;
+import java.util.Stack;
+
+class StackMethods{
+	// add elements to the stack
+	public Stack<Integer> addElements(){
+		Stack<Integer> stackInt = new Stack<>();
+		stackInt.push(24);
+		stackInt.push(05);
+		stackInt.add(2, 1987);
+		return stackInt;
+	}
+}
 
 public class Stacks {
-
+	public static void main(String []args) {
+		
+		// instantiate the class
+		StackMethods stkMethods = new StackMethods();
+		
+		// invoke addElements method
+		Stack<Integer> stkInt = stkMethods.addElements();
+		System.out.println(stkInt);
+		
+	}
 }
