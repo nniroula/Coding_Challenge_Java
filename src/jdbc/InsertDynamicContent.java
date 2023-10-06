c602161c0da7670dfee85fbf96ead87de3751461
nabin <nkbhaie2017@gmail.com>
2023-10-05 20:19:30 -0600

    insert dynamic content into database table

---
+++ src/jdbc/InsertDynamicContent.java
@@ -0,0 +1,53 @@
+package jdbc;
+import java.sql.*;
+
+/*
+ * To create dynamic content, store values to be inserted into database table in variables
+ * in sql query statement, use ? to represent dynamic content
+ * before executing the query in resultset, update the variables with dynamic contents
+ * 
+ * When you use ps.executeUpdate(), you do not have to pass in query for dynamic data insert,
+ * rather you have to update variables with dynamic values before executing it.
+ */
+
+public class InsertDynamicContent {
+	public static void main(String [] args) {
+		System.out.println("JDBC to insert Dynamic content...");
+		
+		// set up for connection to local host
+		String url = "jdbc:mysql://localhost:3306/jdbc_db_one";
+		String username = "root";
+		String password = "Mysql#23";
+		
+		// dynamic content
+		String lang = "Scala"; // for language column in database table
+		String technology = "echo"; // for frontend_tool column in database table
+		
+		// SQL query to insert dynamic content, use ? for dynamic content
+		String insertDynamicContent = "INSERT INTO developer_tools(language, frontend_tool) "
+				+ "VALUES(?, ?)";
+		
+		try {
+			//load mysql jdbc driver
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			// connect to mysql database
+			Connection con = DriverManager.getConnection(url, username, password);
+			// use PreparedStatement for handling dynamic content
+			PreparedStatement ps = con.prepareStatement(insertDynamicContent);
+			//update the dynamic content
+			ps.setString(1, lang);
+			ps.setString(2, technology);
+			// Now, execute the statement
+			int numRowsAffected = ps.executeUpdate();
+			System.out.println("Number of rows affecterd = " + numRowsAffected);
+			
+			// close connection and statement
+			con.close();
+			ps.close();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
